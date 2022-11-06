6bda6e6910dea81d54de10a6a47716ea70ee4337
majaliju <majalijunyc@gmail.com>
2022-11-05 22:35:31 -0400

    stuck on printer method

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.map{|name| badge_maker(name)}
+end
+
+def assign_rooms(array)
+  array.map.each_with_index {|name, index| "Hello, #{name}! You'll be assigned to room #{index+1}!"}
+end
+
+def printer(array)
+  puts batch_badge_creator(array)
+  puts assign_rooms(array)
+end
